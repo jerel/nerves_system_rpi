8d088a4eaf026654edbda4ca5fb5454e4a82d83b
Jerel Unruh <mail@unruhdesigns.com>
2016-07-25 21:01:52 -0500

    Attempting a custom build

--- nerves.exs
+++ nerves.exs
@@ -9,8 +9,7 @@ config :nerves_system_rpi, :nerves_env,
   type: :system,
   version: version,
   mirrors: [
-    "https://github.com/nerves-project/nerves_system_rpi/releases/download/v#{version}/nerves_system_rpi-v#{version}.tar.gz",
-    "https://s3.amazonaws.com/nerves/artifacts/nerves_system_rpi-#{version}.tar.gz"],
+    "https://github.com/jerel/nerves_system_rpi/releases/download/v#{version}/nerves_system_rpi-v#{version}.tar.gz",
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
